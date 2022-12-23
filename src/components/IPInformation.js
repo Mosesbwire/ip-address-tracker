d820249ff52339312d7c1116f36bd64a934a9d57
musa1992 <mosesdev92@gmail.com>
2022-12-23 21:51:50 +0300

    add style for desktop screen

--- src/components/IPInformation.js
+++ src/components/IPInformation.js
@@ -5,15 +5,15 @@ const IPInformation = props => {
   return (
     <div className='information-section'>
       <div className='information-wrapper'>
-        <div>
+        <div className ='border-lg'>
             <p className='subtitles'>IP ADDRESS</p>
             <p className='info-text'>192.212.174.101</p>
         </div>
-        <div>
+        <div className ='border-lg'>
             <p className='subtitles'>LOCATION</p>
             <p className='info-text'>Kasarani, Nairobi</p>
         </div>
-        <div>
+        <div className ='border-lg'>
             <p className='subtitles'>TIMEZONE</p>
             <p className='info-text'>EAT - 15:00</p>
         </div>
